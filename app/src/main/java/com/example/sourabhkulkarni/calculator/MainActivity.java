85f61c30fd632b79d0e665476cb0d2325d82b976
sourabh kulkarni <skulkar6@stevens.edu>
2017-04-10 23:01:17 -0400

    My first app

---
+++ app/src/main/java/com/example/sourabhkulkarni/calculator/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.sourabhkulkarni.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private static Button b1,b2,b3,b4;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+        public void onButtonClick (View v){
+            EditText e1 = (EditText) findViewById(R.id.editText);
+            EditText e2 = (EditText) findViewById(R.id.editText2);
+            TextView t1 = (TextView) findViewById(R.id.editText3);
+
+            int num1 = Integer.parseInt((e1.getText().toString()));
+            int num2 = Integer.parseInt(e2.getText().toString());
+            int sum = num1 + num2;
+            t1.setText(Integer.toString(sum));
+
+
+        }
+    public void onButtonClick1 (View v){
+        EditText e1 = (EditText) findViewById(R.id.editText);
+        EditText e2 = (EditText) findViewById(R.id.editText2);
+        TextView t1 = (TextView) findViewById(R.id.editText3);
+
+        int num1 = Integer.parseInt((e1.getText().toString()));
+        int num2 = Integer.parseInt(e2.getText().toString());
+        int sum = num1 - num2;
+        t1.setText(Integer.toString(sum));
+
+
+    }
+    public void onButtonClick2 (View v){
+        EditText e1 = (EditText) findViewById(R.id.editText);
+        EditText e2 = (EditText) findViewById(R.id.editText2);
+        TextView t1 = (TextView) findViewById(R.id.editText3);
+
+        int num1 = Integer.parseInt((e1.getText().toString()));
+        int num2 = Integer.parseInt(e2.getText().toString());
+        int sum = num1 * num2;
+        t1.setText(Integer.toString(sum));
+
+
+    }
+    public void onButtonClick3 (View v){
+        EditText e1 = (EditText) findViewById(R.id.editText);
+        EditText e2 = (EditText) findViewById(R.id.editText2);
+        TextView t1 = (TextView) findViewById(R.id.editText3);
+
+        int num1 = Integer.parseInt((e1.getText().toString()));
+        int num2 = Integer.parseInt(e2.getText().toString());
+        int sum = num1 / num2;
+        t1.setText(Integer.toString(sum));
+
+
+    }
+    }
+
